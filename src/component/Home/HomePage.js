41f44d728c19c1efbea9611626b803c2a21fc960
NguyenTheToan0609 <thetoan0609@gmail.com>
2024-08-18 14:40:43 +0700

    bai52

--- src/component/Home/HomePage.js
+++ src/component/Home/HomePage.js
@@ -1,5 +1,13 @@
+import videoHomepage from "../../assets/video-homepage.mp4";
+
 const HomePage = () => {
-  return <div className="homepage-container">Home component</div>;
+  return (
+    <div className="homepage-container">
+      <video autoPlay muted loop>
+        <source src={videoHomepage} />
+      </video>
+    </div>
+  );
 };
 
 export default HomePage;
