f0d4bfca7d79cc0fcd9cd1d874517b32e0125b73
NguyenTheToan0609 <thetoan0609@gmail.com>
2024-08-21 18:56:36 +0700

    bai80

--- src/utils/axiosCustomize.js
+++ src/utils/axiosCustomize.js
@@ -22,7 +22,6 @@ instance.interceptors.request.use(
     return Promise.reject(error);
   }
 );
-
 // Add a response interceptor
 instance.interceptors.response.use(
   function (response) {
@@ -32,6 +31,7 @@ instance.interceptors.response.use(
     return response && response.data ? response.data : response;
   },
   function (error) {
+    NProgress.done();
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     return error && error.response && error.response.data
