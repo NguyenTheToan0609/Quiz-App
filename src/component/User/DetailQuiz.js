3c2d0b5d8e73d01f6e844418bc7dc5102598e2d0
NguyenTheToan0609 <thetoan0609@gmail.com>
2024-08-22 12:18:43 +0700

    bai83

--- src/component/User/DetailQuiz.js
+++ src/component/User/DetailQuiz.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiServices";
-
+import _ from "lodash";
 const DetailQuiz = () => {
   const params = useParams();
   const quizId = params.id;
@@ -12,7 +12,31 @@ const DetailQuiz = () => {
 
   const fetchQuestion = async () => {
     const res = await getDataQuiz(quizId);
-    console.log("check res", res);
+    if (res && res.EC === 0) {
+      let raw = res.DT;
+      let data = _.chain(raw)
+        // Group the elements of Array based on `color` property
+        .groupBy("id")
+
+        .map((value, key) => {
+          let answers = [];
+          let questionDescription,
+            image = null;
+          value.forEach((item, index) => {
+            if (index === 0) {
+              questionDescription = item.description;
+              image = item.image;
+            }
+            answers.push(item.answers);
+          });
+          console.log("value :", value, "key", key);
+
+          return { questionID: key, answers, questionDescription, image };
+        })
+        .value();
+      console.log(data);
+      // console.log("check data", data);
+    }
   };
 
   return (
